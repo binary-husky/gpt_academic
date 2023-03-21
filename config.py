787b5be7afd8682f71008edd7be6891cb66399b1
505030475 <505030475@qq.com>
2023-03-21 13:53:24 +0800

    ok

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-# my_api_key = "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"
+# API_KEY = "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" 此key无效
 API_KEY = "sk-此处填API秘钥"
 API_URL = "https://api.openai.com/v1/chat/completions"
 
@@ -9,3 +9,7 @@ if USE_PROXY:
 else:
     proxies = None
     print('网络代理状态：未配置。无代理状态下很可能无法访问。')
+
+# avoid dummy
+if API_KEY == "sk-此处填API秘钥":
+    assert False, "请在config文件中修改API密钥, 添加海外代理之后再运行"
