e8d86a3242f8bd1e8d4a3b5e61f28a839bfbf2b1
qingxu fu <505030475@qq.com>
2023-03-22 17:45:10 +0800

    代理位置

--- check_proxy.py
+++ check_proxy.py
@@ -9,8 +9,8 @@ def check_proxy(proxies):
     try:
         response = requests.get("https://ipapi.co/json/", proxies=proxies, timeout=4)
         data = response.json()
+        print(f'查询代理的地理位置，返回的结果是{data}')
         country = data['country_name']
-        # city = data['city']
         result = f"代理配置 {proxies_https}, 代理所在地：{country}"
         print(result)
         return result
