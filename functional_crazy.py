2f4c46b22c66fe92a212f3843344280d944dfea5
qingxu fu <505030475@qq.com>
2023-03-24 20:56:34 +0800

    trim button text

--- functional_crazy.py
+++ functional_crazy.py
@@ -8,26 +8,26 @@ def get_crazy_functionals():
     from crazy_functions.高级功能函数模板 import 高阶功能模板函数
 
     return {
-        "[实验功能] 请解析并解构此项目本身": {
+        "[实验] 请解析并解构此项目本身": {
             "Function": 解析项目本身
         },
-        "[实验功能] 解析整个Python项目（input输入项目根路径）": {
+        "[实验] 解析整个py项目（input输入项目根路径）": {
             "Color": "stop",    # 按钮颜色
             "Function": 解析一个Python项目
         },
-        "[实验功能] 解析整个C++项目的头文件（input输入项目根路径）": {
+        "[实验] 解析整个C++项目（input输入项目根路径）": {
             "Color": "stop",    # 按钮颜色
             "Function": 解析一个C项目的头文件
         },
-        "[实验功能] 解读latex论文写摘要（input输入项目根路径）": {
+        "[实验] 读tex论文写摘要（input输入项目根路径）": {
             "Color": "stop",    # 按钮颜色
             "Function": 读文章写摘要
         },
-        "[实验功能] 批量生成函数注释（input输入项目根路径）": {
+        "[实验] 批量生成函数注释（input输入项目根路径）": {
             "Color": "stop",    # 按钮颜色
             "Function": 批量生成函数注释
         },
-        "[实验功能] 实验功能函数模板": {
+        "[实验] 实验功能函数模板": {
             "Color": "stop",    # 按钮颜色
             "Function": 高阶功能模板函数
         },
