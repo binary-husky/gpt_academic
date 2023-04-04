c40f6f00bbb3776a8f4b356ce738b4b925b1af2e
qingxu fu <505030475@qq.com>
2023-04-04 22:54:08 +0800

    check_new_version

--- check_proxy.py
+++ check_proxy.py
@@ -19,6 +19,30 @@ def check_proxy(proxies):
         return result
 
 
+def auto_update():
+    from toolbox import get_conf
+    import requests, time, json
+    proxies, = get_conf('proxies')
+    response = requests.get("https://raw.githubusercontent.com/binary-husky/chatgpt_academic/master/version", 
+                            proxies=proxies, timeout=1)
+    remote_json_data = json.loads(response.text)
+    remote_version = remote_json_data['version']
+    if remote_json_data["show_feature"]:
+        new_feature = "新功能：" + remote_json_data["new_feature"]
+    else:
+        new_feature = ""
+    with open('./version', 'r', encoding='utf8') as f: 
+        current_version = f.read()
+        current_version = json.loads(current_version)['version']
+    if (remote_version - current_version) >= 0.05:
+        print(f'\n新版本可用。新版本:{remote_version}，当前版本:{current_version}。{new_feature}')
+        print('Github更新地址:\nhttps://github.com/binary-husky/chatgpt_academic\n')
+        time.sleep(3)
+        return
+    else:
+        return
+
+
 if __name__ == '__main__':
     import os; os.environ['no_proxy'] = '*' # 避免代理网络产生意外污染
     from toolbox import get_conf
