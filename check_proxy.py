8c21ada50cc2d6ed149791fc171178d90bffd541
qingxu fu <505030475@qq.com>
2023-04-06 18:29:49 +0800

    主要代码规整化

--- check_proxy.py
+++ check_proxy.py
@@ -3,7 +3,8 @@ def check_proxy(proxies):
     import requests
     proxies_https = proxies['https'] if proxies is not None else '无'
     try:
-        response = requests.get("https://ipapi.co/json/", proxies=proxies, timeout=4)
+        response = requests.get("https://ipapi.co/json/",
+                                proxies=proxies, timeout=4)
         data = response.json()
         print(f'查询代理的地理位置，返回的结果是{data}')
         if 'country_name' in data:
@@ -21,9 +22,11 @@ def check_proxy(proxies):
 
 def auto_update():
     from toolbox import get_conf
-    import requests, time, json
+    import requests
+    import time
+    import json
     proxies, = get_conf('proxies')
-    response = requests.get("https://raw.githubusercontent.com/binary-husky/chatgpt_academic/master/version", 
+    response = requests.get("https://raw.githubusercontent.com/binary-husky/chatgpt_academic/master/version",
                             proxies=proxies, timeout=1)
     remote_json_data = json.loads(response.text)
     remote_version = remote_json_data['version']
@@ -31,11 +34,12 @@ def auto_update():
         new_feature = "新功能：" + remote_json_data["new_feature"]
     else:
         new_feature = ""
-    with open('./version', 'r', encoding='utf8') as f: 
+    with open('./version', 'r', encoding='utf8') as f:
         current_version = f.read()
         current_version = json.loads(current_version)['version']
     if (remote_version - current_version) >= 0.05:
-        print(f'\n新版本可用。新版本:{remote_version}，当前版本:{current_version}。{new_feature}')
+        print(
+            f'\n新版本可用。新版本:{remote_version}，当前版本:{current_version}。{new_feature}')
         print('Github更新地址:\nhttps://github.com/binary-husky/chatgpt_academic\n')
         time.sleep(3)
         return
@@ -44,8 +48,8 @@ def auto_update():
 
 
 if __name__ == '__main__':
-    import os; os.environ['no_proxy'] = '*' # 避免代理网络产生意外污染
+    import os
+    os.environ['no_proxy'] = '*'  # 避免代理网络产生意外污染
     from toolbox import get_conf
     proxies, = get_conf('proxies')
     check_proxy(proxies)
-    
