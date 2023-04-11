3084c03e55ba964ba07e6fd76d43f915ea884430
qingxu fu <505030475@qq.com>
2023-04-11 18:50:56 +0800

    UP

--- check_proxy.py
+++ check_proxy.py
@@ -113,7 +113,7 @@ def auto_update():
         with open('./version', 'r', encoding='utf8') as f:
             current_version = f.read()
             current_version = json.loads(current_version)['version']
-        if (remote_version - current_version) >= 0.05:
+        if (remote_version - current_version) >= 0.01:
             from colorful import print亮黄
             print亮黄(
                 f'\n新版本可用。新版本:{remote_version}，当前版本:{current_version}。{new_feature}')
