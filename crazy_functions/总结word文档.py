e467ab2d3f50391fb594352a55b801f12158fbfd
qingxu fu <505030475@qq.com>
2023-04-11 19:58:37 +0800

    2.67 修复typo

--- crazy_functions/总结word文档.py
+++ crazy_functions/总结word文档.py
@@ -52,7 +52,7 @@ def 解析docx(file_manifest, project_folder, llm_kwargs, plugin_kwargs, chatbot
             chatbot[-1] = (i_say_show_user, gpt_say)
             history.append(i_say_show_user)
             history.append(gpt_say)
-            yield from update_ui(chatbot=chatbot, history=chatbot, msg=msg) # 刷新界面
+            yield from update_ui(chatbot=chatbot, history=history, msg=msg) # 刷新界面
             if not fast_debug: time.sleep(2)
 
     """
@@ -84,10 +84,10 @@ def 解析docx(file_manifest, project_folder, llm_kwargs, plugin_kwargs, chatbot
         chatbot[-1] = (i_say, gpt_say)
         history.append(i_say)
         history.append(gpt_say)
-        yield from update_ui(chatbot=chatbot, history=chatbot, msg=msg) # 刷新界面
+        yield from update_ui(chatbot=chatbot, history=history, msg=msg) # 刷新界面
         res = write_results_to_file(history)
         chatbot.append(("完成了吗？", res))
-        yield from update_ui(chatbot=chatbot, history=chatbot, msg=msg) # 刷新界面
+        yield from update_ui(chatbot=chatbot, history=history, msg=msg) # 刷新界面
 
 
 @CatchException
--- crazy_functions/读文章写摘要.py
+++ crazy_functions/读文章写摘要.py
@@ -24,7 +24,7 @@ def 解析Paper(file_manifest, project_folder, llm_kwargs, plugin_kwargs, chatbo
 
             chatbot[-1] = (i_say_show_user, gpt_say)
             history.append(i_say_show_user); history.append(gpt_say)
-            yield from update_ui(chatbot=chatbot, history=chatbot, msg=msg) # 刷新界面
+            yield from update_ui(chatbot=chatbot, history=history, msg=msg) # 刷新界面
             if not fast_debug: time.sleep(2)
 
     all_file = ', '.join([os.path.relpath(fp, project_folder) for index, fp in enumerate(file_manifest)])
@@ -39,10 +39,10 @@ def 解析Paper(file_manifest, project_folder, llm_kwargs, plugin_kwargs, chatbo
 
         chatbot[-1] = (i_say, gpt_say)
         history.append(i_say); history.append(gpt_say)
-        yield from update_ui(chatbot=chatbot, history=chatbot, msg=msg) # 刷新界面
+        yield from update_ui(chatbot=chatbot, history=history, msg=msg) # 刷新界面
         res = write_results_to_file(history)
         chatbot.append(("完成了吗？", res))
-        yield from update_ui(chatbot=chatbot, history=chatbot, msg=msg) # 刷新界面
+        yield from update_ui(chatbot=chatbot, history=history, msg=msg) # 刷新界面
 
 
 
