bc3f3429a5b15e4393c3dd323e422ee68229246a
qingxu fu <505030475@qq.com>
2023-04-14 11:57:27 +0800

    源代码文件数量限制 1024 个

--- crazy_functions/解析项目源代码.py
+++ crazy_functions/解析项目源代码.py
@@ -12,7 +12,7 @@ def 解析源代码新(file_manifest, project_folder, llm_kwargs, plugin_kwargs,
     sys_prompt_array = []
     report_part_1 = []
     
-    assert len(file_manifest) <= 512, "源文件太多, 请缩减输入文件的数量, 或者删除此行并拆分file_manifest以保证结果能被分批存储。"
+    assert len(file_manifest) <= 1024, "源文件太多, 请缩减输入文件的数量, 或者删除此行并拆分file_manifest以保证结果能被分批存储。"
     ############################## <第一步，逐个文件分析，多线程> ##################################
     for index, fp in enumerate(file_manifest):
         with open(fp, 'r', encoding='utf-8', errors='replace') as f:
