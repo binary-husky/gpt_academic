92501769675be02e0f8b12494379ab542ace20b0
Your Name <you@example.com>
2023-04-19 14:49:34 +0800

    添加测试

--- config.py
+++ config.py
@@ -44,7 +44,7 @@ WEB_PORT = -1
 MAX_RETRY = 2
 
 # OpenAI模型选择是（gpt4现在只对申请成功的人开放）
-LLM_MODEL = "gpt-3.5-turbo" # 可选 "chatglm"
+LLM_MODEL = "gpt-3.5-turbo" # 可选 ↓↓↓
 AVAIL_LLM_MODELS = ["gpt-3.5-turbo", "api2d-gpt-3.5-turbo", "gpt-4", "api2d-gpt-4", "chatglm"]
 
 # 本地LLM模型如ChatGLM的执行方式 CPU/GPU
