4065d6e234869c7261fb14a923f6782002508b3e
binary-husky <505030475@qq.com>
2023-04-22 18:56:02 +0800

    版本3.2

--- crazy_functional.py
+++ crazy_functional.py
@@ -207,6 +207,15 @@ def get_crazy_functions():
             "Function": HotReload(解析任意code项目)
         },
     })
-
+    from crazy_functions.询问多个大语言模型 import 同时问询_指定模型
+    function_plugins.update({
+        "询问多个GPT模型（手动指定询问哪些模型）": {
+            "Color": "stop",
+            "AsButton": False,
+            "AdvancedArgs": True, # 调用时，唤起高级参数输入区（默认False）
+            "ArgsReminder": "支持任意数量的llm接口，用&符号分隔。例如chatglm&gpt-3.5-turbo&api2d-gpt-4", # 高级参数输入区的显示提示
+            "Function": HotReload(同时问询_指定模型)
+        },
+    })
     ###################### 第n组插件 ###########################
     return function_plugins
