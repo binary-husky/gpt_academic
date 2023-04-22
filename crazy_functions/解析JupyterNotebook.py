880be21bf713bfff940e81f0afebdc8fff98c88e
Your Name <you@example.com>
2023-04-22 16:19:36 +0800

    Add test for juptyer notebook plugin

--- crazy_functions/解析JupyterNotebook.py
+++ crazy_functions/解析JupyterNotebook.py
@@ -1,5 +1,5 @@
 from toolbox import update_ui
-from toolbox import CatchException, report_execption
+from toolbox import CatchException, report_execption, write_results_to_file
 fast_debug = True
 
 
@@ -79,9 +79,9 @@ def ipynb解释(file_manifest, project_folder, llm_kwargs, plugin_kwargs, chatbo
     pfg.run_file_split(max_token_limit=1024)
     n_split = len(pfg.sp_file_contents)
 
-    inputs_array = [f"This is a  Jupyter Notebook file, tell me about Each Block in Chinese. Focus Just On Code." +
-                    f"If a block starts with `Markdown` which means it's a markdown block in ipynbipynb. " +
-                    f"Start a new line for a block and block num use Chinese." +
+    inputs_array = [r"This is a Jupyter Notebook file, tell me about Each Block in Chinese. Focus Just On Code." +
+                    r"If a block starts with `Markdown` which means it's a markdown block in ipynbipynb. " +
+                    r"Start a new line for a block and block num use Chinese." +
                     f"\n\n{frag}" for frag in pfg.sp_file_contents]
     inputs_show_user_array = [f"{f}的分析如下" for f in pfg.sp_file_tag]
     sys_prompt_array = ["You are a professional programmer."] * n_split
@@ -100,8 +100,13 @@ def ipynb解释(file_manifest, project_folder, llm_kwargs, plugin_kwargs, chatbo
     #  <-------- 整理结果，退出 ---------->
     block_result = "  \n".join(gpt_response_collection)
     chatbot.append(("解析的结果如下", block_result))
+    history.extend(["解析的结果如下", block_result])
     yield from update_ui(chatbot=chatbot, history=history)  # 刷新界面
 
+    #  <-------- 写入文件，退出 ---------->
+    res = write_results_to_file(history)
+    chatbot.append(("完成了吗？", res))
+    yield from update_ui(chatbot=chatbot, history=history) # 刷新界面
 
 @CatchException
 def 解析ipynb文件(txt, llm_kwargs, plugin_kwargs, chatbot, history, system_prompt, web_port):
