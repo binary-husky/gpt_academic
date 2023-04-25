d3d0016361d38ca29ee91a72df8aff9eb564c5ed
bbehindgfw <71253376+bbehindgfw@users.noreply.github.com>
2023-04-25 22:53:41 +0800

    Update config.py
    更改代理协议格式

--- config.py
+++ config.py
@@ -5,16 +5,16 @@ API_KEY = "sk-此处填API密钥"    # 可同时填写多个API-KEY，用英文�
 USE_PROXY = False
 if USE_PROXY:
     # 填写格式是 [协议]://  [地址] :[端口]，填写之前不要忘记把USE_PROXY改成True，如果直接在海外服务器部署，此处不修改
-    # 例如    "socks5h://localhost:11284"
-    # [协议] 常见协议无非socks5h/http; 例如 v2**y 和 ss* 的默认本地协议是socks5h; 而cl**h 的默认本地协议是http
+    # 例如    "socks5://localhost:11284"
+    # [协议] 常见协议无非socks5/http; 例如 v2**y 和 ss* 的默认本地协议是socks5h; 而cl**h 的默认本地协议是http
     # [地址] 懂的都懂，不懂就填localhost或者127.0.0.1肯定错不了（localhost意思是代理软件安装在本机上）
     # [端口] 在代理软件的设置里找。虽然不同的代理软件界面不一样，但端口号都应该在最显眼的位置上
 
     # 代理网络的地址，打开你的科学上网软件查看代理的协议(socks5/http)、地址(localhost)和端口(11284)
     proxies = {
         #          [协议]://  [地址]  :[端口]
-        "http":  "socks5h://localhost:11284",
-        "https": "socks5h://localhost:11284",
+        "http":  "socks5://localhost:11284",
+        "https": "socks5://localhost:11284",
     }
 else:
     proxies = None
