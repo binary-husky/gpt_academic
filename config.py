2dbf06e1cf6b86cbfe51fc79eda41d5a9c3ce4fc
yushuo99454 <131931898+yushuo99454@users.noreply.github.com>
2023-04-27 12:19:57 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # [step 1]>> 例如： API_KEY = "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" （此key无效）
-API_KEY = "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"    # 可同时填写多个API-KEY，用英文逗号分割，例如API_KEY = "sk-openaikey1,sk-openaikey2,fkxxxx-api2dkey1,fkxxxx-api2dkey2"
+API_KEY = "sk-1"    # 可同时填写多个API-KEY，用英文逗号分割，例如API_KEY = "sk-openaikey1,sk-openaikey2,fkxxxx-api2dkey1,fkxxxx-api2dkey2"
 
 # [step 2]>> 改为True应用代理，如果直接在海外服务器部署，此处不修改
 USE_PROXY = False
