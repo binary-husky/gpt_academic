33cd7068fb768ccfc1da818c7167063bfcd6e1a1
binary-husky <96192199+binary-husky@users.noreply.github.com>
2023-05-01 23:28:28 +0800

    Update config.py

--- config.py
+++ config.py
@@ -13,8 +13,8 @@ if USE_PROXY:
     # 代理网络的地址，打开你的*学*网软件查看代理的协议(socks5/http)、地址(localhost)和端口(11284)
     proxies = {
         #          [协议]://  [地址]  :[端口]
-        "http":  "socks5h://localhost:11284",
-        "https": "socks5h://localhost:11284",
+        "http":  "socks5h://localhost:11284",  # 再例如  "http":  "http://127.0.0.1:7890",
+        "https": "socks5h://localhost:11284",  # 再例如  "https": "http://127.0.0.1:7890",
     }
 else:
     proxies = None
