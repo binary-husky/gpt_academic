2ee7e6580231c030ac5ca8c00a526620b1fcdc49
binary-husky <qingxu.fu@outlook.com>
2023-05-04 01:36:09 +0800

    修改提示

--- crazy_functional.py
+++ crazy_functional.py
@@ -70,7 +70,7 @@ def get_crazy_functions():
             "AsButton": False,  # 加入下拉菜单中
             "Function": HotReload(解析一个Java项目)
         },
-        "解析整个前端项目": {
+        "解析整个前端项目（js,ts,css等）": {
             "Color": "stop",  # 按钮颜色
             "AsButton": False,  # 加入下拉菜单中
             "Function": HotReload(解析一个前端项目)
