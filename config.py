ca6b93655932aae7649656318dd00831ac97ee3f
XiaojianTang <277172719@qq.com>
2023-05-26 20:37:06 +0800

    增加对azure openai api的支持

--- config.py
+++ config.py
@@ -1,6 +1,13 @@
 # [step 1]>> 例如： API_KEY = "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" （此key无效）
 API_KEY = "sk-此处填API密钥"    # 可同时填写多个API-KEY，用英文逗号分割，例如API_KEY = "sk-openaikey1,sk-openaikey2,fkxxxx-api2dkey1,fkxxxx-api2dkey2"
 
+
+AZURE_ENDPOINT = "https://gacopenai01.openai.azure.com/"
+AZURE_API_KEY = "1c5420bc760f4257a6ef6fb80de47ca7"
+AZURE_API_VERSION = "2023-05-15"
+AZURE_ENGINE = "gac_gpt-35"
+
+
 # [step 2]>> 改为True应用代理，如果直接在海外服务器部署，此处不修改
 USE_PROXY = False
 if USE_PROXY:
