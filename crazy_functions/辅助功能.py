1ec90f1d0451d2fef542bba670304b8fe6f96972
qingxu fu <505030475@qq.com>
2023-08-16 22:49:46 +0800

    添加本地缓存删除功能

--- crazy_functions/辅助功能.py
+++ crazy_functions/辅助功能.py
@@ -3,7 +3,7 @@
 # @Author : Spike
 # @Descr   :
 from toolbox import update_ui
-from toolbox import CatchException, report_execption, write_results_to_file
+from toolbox import CatchException, report_execption, write_results_to_file, get_log_folder
 from crazy_functions.crazy_utils import request_gpt_model_in_new_thread_with_ui_alive
 
 
@@ -25,4 +25,19 @@ def 猜你想问(txt, llm_kwargs, plugin_kwargs, chatbot, history, system_prompt
     )
     chatbot[-1] = (show_say, gpt_say)
     history.extend([show_say, gpt_say])
+    yield from update_ui(chatbot=chatbot, history=history)  # 刷新界面
+
+
+@CatchException
+def 清除缓存(txt, llm_kwargs, plugin_kwargs, chatbot, history, system_prompt, web_port):
+    chatbot.append(['清除本地缓存数据', '执行中. 删除 gpt_log & private_upload'])
+    yield from update_ui(chatbot=chatbot, history=history)  # 刷新界面
+
+    import shutil, os
+    gpt_log_dir = os.path.join(os.path.dirname(__file__), '..', 'gpt_log')
+    private_upload_dir = os.path.join(os.path.dirname(__file__), '..', 'private_upload')
+    shutil.rmtree(gpt_log_dir, ignore_errors=True)
+    shutil.rmtree(private_upload_dir, ignore_errors=True)
+
+    chatbot.append(['清除本地缓存数据', '执行完成'])
     yield from update_ui(chatbot=chatbot, history=history)  # 刷新界面
