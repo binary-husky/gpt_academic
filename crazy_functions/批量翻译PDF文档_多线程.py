cce69beee9102a901d6208d7c3fa295aa2e5ff09
qingxu fu <505030475@qq.com>
2023-09-08 11:08:02 +0800

    update error message

--- crazy_functions/批量翻译PDF文档_多线程.py
+++ crazy_functions/批量翻译PDF文档_多线程.py
@@ -66,7 +66,7 @@ def 解析PDF_基于GROBID(file_manifest, project_folder, llm_kwargs, plugin_kwa
     for index, fp in enumerate(file_manifest):
         chatbot.append(["当前进度：", f"正在连接GROBID服务，请稍候: {grobid_url}\n如果等待时间过长，请修改config中的GROBID_URL，可修改成本地GROBID服务。"]); yield from update_ui(chatbot=chatbot, history=history) # 刷新界面
         article_dict = parse_pdf(fp, grobid_url)
-        print(article_dict)
+        if article_dict is None: raise RuntimeError("解析PDF失败，请检查PDF是否损坏。")
         prompt = "以下是一篇学术论文的基本信息:\n"
         # title
         title = article_dict.get('title', '无法获取 title'); prompt += f'title:{title}\n\n'
