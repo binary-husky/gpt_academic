87f629bb3703d05f2f674a35dfdae75f2a626775
binary-husky <qingxu.fu@outlook.com>
2023-09-23 20:24:13 +0800

    添加gpt-4-32k

--- request_llm/bridge_all.py
+++ request_llm/bridge_all.py
@@ -125,7 +125,14 @@ model_info = {
         "tokenizer": tokenizer_gpt4,
         "token_cnt": get_token_num_gpt4,
     },
-
+    "gpt-4-32k": {
+        "fn_with_ui": chatgpt_ui,
+        "fn_without_ui": chatgpt_noui,
+        "endpoint": openai_endpoint,
+        "max_token": 32768,
+        "tokenizer": tokenizer_gpt4,
+        "token_cnt": get_token_num_gpt4,
+    },
     # azure openai
     "azure-gpt-3.5":{
         "fn_with_ui": chatgpt_ui,
