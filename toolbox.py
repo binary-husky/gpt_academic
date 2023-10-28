65dab46a286e28285983b79ef0653188d6eb3644
binary-husky <qingxu.fu@outlook.com>
2023-10-29 00:47:48 +0800

    api_key_manager

--- toolbox.py
+++ toolbox.py
@@ -732,7 +732,11 @@ def select_api_key(keys, llm_model):
         raise RuntimeError(f"您提供的api-key不满足要求，不包含任何可用于{llm_model}的api-key。您可能选择了错误的模型或请求源（右下角更换模型菜单中可切换openai,azure,claude,api2d等请求源）。")
 
     api_key = random.choice(avail_key_list) # 随机负载均衡
-    if ENABLE
+
+    if get_conf("BLOCK_INVALID_APIKEY"): 
+        # 实验性功能，自动检测并屏蔽失效的KEY，请勿使用
+        from request_llms.key_manager import ApiKeyManager
+        api_key = ApiKeyManager().select_avail_key(avail_key_list)
     return api_key
 
 def read_env_variable(arg, default_value):
