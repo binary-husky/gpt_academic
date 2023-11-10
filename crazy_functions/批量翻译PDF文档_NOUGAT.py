362b545a45352b011adef023e54f9c34a8110fdf
xiangsam <huang.xiang@buaa.edu.cn>
2023-11-10 14:25:37 +0000

    更改import nougat时机

--- crazy_functions/批量翻译PDF文档_NOUGAT.py
+++ crazy_functions/批量翻译PDF文档_NOUGAT.py
@@ -57,22 +57,22 @@ def 批量翻译PDF文档(txt, llm_kwargs, plugin_kwargs, chatbot, history, syst
         "批量翻译PDF文档。函数插件贡献者: Binary-Husky"])
     yield from update_ui(chatbot=chatbot, history=history) # 刷新界面
 
-    # 尝试导入依赖，如果缺少依赖，则给出安装建议
-    try:
-        import nougat
-        import tiktoken
-    except:
-        report_execption(chatbot, history,
-                         a=f"解析项目: {txt}",
-                         b=f"导入软件依赖失败。使用该模块需要额外依赖，安装方法```pip install --upgrade nougat-ocr tiktoken```。")
-        yield from update_ui(chatbot=chatbot, history=history) # 刷新界面
-        return
-
     # 清空历史，以免输入溢出
     history = []
 
     from .crazy_utils import get_files_from_everything
     success, file_manifest, project_folder = get_files_from_everything(txt, type='.pdf')
+    if len(file_manifest) > 0:
+        # 尝试导入依赖，如果缺少依赖，则给出安装建议
+        try:
+            import nougat
+            import tiktoken
+        except:
+            report_execption(chatbot, history,
+                             a=f"解析项目: {txt}",
+                             b=f"导入软件依赖失败。使用该模块需要额外依赖，安装方法```pip install --upgrade nougat-ocr tiktoken```。")
+            yield from update_ui(chatbot=chatbot, history=history) # 刷新界面
+            return
     success_mmd, file_manifest_mmd, _ = get_files_from_everything(txt, type='.mmd')
     success = success or success_mmd
     file_manifest += file_manifest_mmd
