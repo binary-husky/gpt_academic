439147e4b77732a845b2ec61fac9bef1293d3cbb
binary-husky <qingxu.fu@outlook.com>
2023-12-17 15:55:15 +0800

    re-arrange main.py

--- themes/theme.py
+++ themes/theme.py
@@ -1,6 +1,14 @@
-import gradio as gr
+import pickle
+import base64
+import uuid
 from toolbox import get_conf
-THEME = get_conf('THEME')
+
+"""
+-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+第 1 部分
+加载主题相关的工具函数
+-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+"""
 
 def load_dynamic_theme(THEME):
     adjust_dynamic_theme = None
@@ -20,4 +28,86 @@ def load_dynamic_theme(THEME):
         theme_declaration = ""
     return adjust_theme, advanced_css, theme_declaration, adjust_dynamic_theme
 
-adjust_theme, advanced_css, theme_declaration, _ = load_dynamic_theme(THEME)
+adjust_theme, advanced_css, theme_declaration, _ = load_dynamic_theme(get_conf('THEME'))
+
+
+
+
+
+
+"""
+-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+第 2 部分
+cookie相关工具函数
+-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+"""
+
+def init_cookie(cookies, chatbot):
+    # 为每一位访问的用户赋予一个独一无二的uuid编码
+    cookies.update({'uuid': uuid.uuid4()})
+    return cookies
+
+def to_cookie_str(d):
+    # Pickle the dictionary and encode it as a string
+    pickled_dict = pickle.dumps(d)
+    cookie_value = base64.b64encode(pickled_dict).decode('utf-8')
+    return cookie_value
+
+def from_cookie_str(c):
+    # Decode the base64-encoded string and unpickle it into a dictionary
+    pickled_dict = base64.b64decode(c.encode('utf-8'))
+    return pickle.loads(pickled_dict)
+
+
+
+
+
+"""
+-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+第 3 部分
+内嵌的javascript代码
+-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+"""
+
+js_code_for_css_changing = """(css) => {
+    var existingStyles = document.querySelectorAll("style[data-loaded-css]");
+    for (var i = 0; i < existingStyles.length; i++) {
+        var style = existingStyles[i];
+        style.parentNode.removeChild(style);
+    }
+    var styleElement = document.createElement('style');
+    styleElement.setAttribute('data-loaded-css', css);
+    styleElement.innerHTML = css;
+    document.head.appendChild(styleElement);
+}
+"""
+
+js_code_for_darkmode_init = """(dark) => {
+    dark = dark == "True";
+    if (document.querySelectorAll('.dark').length) {
+        if (!dark){
+            document.querySelectorAll('.dark').forEach(el => el.classList.remove('dark'));
+        }
+    } else {
+        if (dark){
+            document.querySelector('body').classList.add('dark');
+        }
+    }
+}
+"""
+
+js_code_for_toggle_darkmode = """() => {
+    if (document.querySelectorAll('.dark').length) {
+        document.querySelectorAll('.dark').forEach(el => el.classList.remove('dark'));
+    } else {
+        document.querySelector('body').classList.add('dark');
+    }
+}"""
+
+
+js_code_for_persistent_cookie_init = """(persistent_cookie) => {
+    return getCookie("persistent_cookie");
+}
+"""
+
+
