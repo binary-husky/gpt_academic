cbbfa2e586f49865ba8a21a544ebdf03b410e29b
binary-husky <96192199+binary-husky@users.noreply.github.com>
2024-03-02 23:14:46 +0800

    update gpt 0125

--- request_llms/bridge_all.py
+++ request_llms/bridge_all.py
@@ -116,7 +116,16 @@ model_info = {
         "tokenizer": tokenizer_gpt35,
         "token_cnt": get_token_num_gpt35,
     },
-
+    
+    "gpt-3.5-turbo-0125": {
+        "fn_with_ui": chatgpt_ui,
+        "fn_without_ui": chatgpt_noui,
+        "endpoint": openai_endpoint,
+        "max_token": 4096,
+        "tokenizer": tokenizer_gpt35,
+        "token_cnt": get_token_num_gpt35,
+    },
+    
     "gpt-3.5-turbo-16k-0613": {
         "fn_with_ui": chatgpt_ui,
         "fn_without_ui": chatgpt_noui,
