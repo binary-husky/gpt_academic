26d725c14f27359bb19c66d23ee678e9abd96eb5
spike <2411123479@qq.com>
2024-03-07 16:31:33 +0800

    pdf loader

--- common/document_loaders/__init__.py
+++ common/document_loaders/__init__.py
@@ -2,6 +2,7 @@ from .mypdfloader import RapidOCRPDFLoader
 from .myimgloader import RapidOCRLoader
 from .mydocloader import RapidOCRDocLoader
 from .mypptloader import RapidOCRPPTLoader
+from .spikepdfloader import ReaderPDFEve
 from .spikedocxloader import ReaderDocxEve
 from .spikemdloader import ReaderMarkdownEve
 from .spikeimgloader import ReaderIMGEve
