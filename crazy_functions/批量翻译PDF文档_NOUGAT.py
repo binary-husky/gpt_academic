382d8877330730fac242b8bddd11e9c4f418a22c
Menghuan <Menghuan2003@outlook.com>
2024-03-11 22:42:51 +0800

    实现将图片插入到识别后pdf中

--- crazy_functions/批量翻译PDF文档_NOUGAT.py
+++ crazy_functions/批量翻译PDF文档_NOUGAT.py
@@ -1,6 +1,6 @@
 from toolbox import CatchException, report_exception, get_log_folder, gen_time_str
 from toolbox import update_ui, promote_file_to_downloadzone, update_ui_lastest_msg, disable_auto_promotion
-from toolbox import write_history_to_file, promote_file_to_downloadzone
+from toolbox import write_history_to_file, zip_result
 from .crazy_utils import request_gpt_model_in_new_thread_with_ui_alive
 from .crazy_utils import request_gpt_model_multi_threads_with_very_awesome_ui_and_high_efficiency
 from .crazy_utils import read_and_clean_pdf_text
@@ -76,13 +76,14 @@ def 按图片拆分文档(input_pdf_path):
         page_ranges.append((current_range_start, len(doc) - 1))
 
     files_splited = []
-
+    if not os.path.exists(input_pdf_path.split('.')[0]):
+        os.makedirs(input_pdf_path.split('.')[0])
     for start, end in page_ranges:
         output_doc = fitz.open()
         for page_number in range(start, end + 1):
             output_doc.insert_pdf(doc, from_page=page_number, to_page=page_number)
         
-        output_pdf_path = f"{input_pdf_path.split('.')[0]}_{start + 1}_to_{end + 1}.pdf"
+        output_pdf_path = f"{input_pdf_path.split('.')[0]}/{start + 1}_to_{end + 1}.pdf"
         output_doc.save(output_pdf_path)
         
         # 对于每个页面范围，检查并保存图片及相关文本
@@ -91,7 +92,7 @@ def 按图片拆分文档(input_pdf_path):
             images = page.get_images(full=True)
             text_blocks = page.get_text("blocks")
             if images:
-                image_folder = f"{input_pdf_path.split('.')[0]}_{start + 1}_to_{end + 1}_images"
+                image_folder = f"{input_pdf_path.split('.')[0]}/{start + 1}_to_{end + 1}_images"
                 if not os.path.exists(image_folder):
                     os.makedirs(image_folder)
                 for img_index, img in enumerate(images):
@@ -158,7 +159,14 @@ def 模糊匹配添加图片(markdown_file_path, pdf_file_path):
                     if insert_index < 0:
                         insert_index = 1
                     break
-            
+
+            # 重构路径方便后续编辑
+            # print(f"1:{find_file_name}")
+            find_file_path = f"{image_folder}/{find_file_name}"
+            image_file_path_components = find_file_path[:-5].split('/')
+            # print(f"2:{image_file_path_components}")
+            image_file_path = "./images/" + "/".join(image_file_path_components[-2:])
+            # print(f"3:{image_file_path}")
             if insert_index is not None:
                 image_markdown = f"\n![Image]({image_file_path})\n"
                 markdown_content.insert(insert_index, image_markdown)
@@ -166,7 +174,7 @@ def 模糊匹配添加图片(markdown_file_path, pdf_file_path):
                 # 如果实在没找到，没办法了只能放在最后
                 image_markdown = f"\n![Image]({image_file_path})\n"
                 markdown_content.append(image_markdown)
-                print("Not find!")
+                logging.info(f"Fail to find place of {image_file_path}")
     
     # 保存更新后的Markdown文档
     with open(markdown_file_path, 'w', encoding='utf-8') as md_file:
@@ -202,10 +210,11 @@ def 批量翻译PDF文档(txt, llm_kwargs, plugin_kwargs, chatbot, history, syst
         try:
             import fitz
             import fuzzywuzzy
+            import pypandoc
         except:
             report_exception(chatbot, history,
                              a=f"解析项目: {txt}",
-                             b=f"导入软件依赖失败。使用该模块需要额外依赖，安装方法```pip install --upgrade PyMuPDF fuzzywuzzy```。")
+                             b=f"导入软件依赖失败。使用该模块需要额外依赖，安装方法```pip install --upgrade PyMuPDF fuzzywuzzy pypandoc_binary```。")
             yield from update_ui(chatbot=chatbot, history=history)
             return
     success_mmd, file_manifest_mmd, _ = get_files_from_everything(txt, type='.mmd')
@@ -228,8 +237,34 @@ def 批量翻译PDF文档(txt, llm_kwargs, plugin_kwargs, chatbot, history, syst
     # 开始正式执行任务
     yield from 解析PDF_基于NOUGAT(file_manifest, project_folder, llm_kwargs, split, chatbot, history, system_prompt)
 
-
-
+def 编译pdf(image_zip, markdown_file, chatbot, generated_conclusion_files):
+    import pypandoc
+    import zipfile
+    import shutil
+    # 将图片压缩包解压
+    with zipfile.ZipFile(image_zip, 'r') as zip_ref:
+        zip_ref.extractall(f"{os.path.dirname(image_zip)}/images")
+    # 将markdown文件编译为pdf,但是转换PDF需要LaTeX环境，保留一个word版本
+    output_file = f"{markdown_file.split('.')[0]}.pdf"
+    print(f"output_file:{output_file}")
+    try:
+        pypandoc.convert_file(markdown_file, 'pdf', outputfile=output_file, extra_args=[f'--resource-path={os.path.dirname(image_zip)}','-V', 'geometry:margin=1in'])
+        logging.info(f"Converted the markdowm to {output_file}")
+    except Exception as e:
+        print亮红(f"未能转换为PDF，这可能是由于未配置Tex环境造成的，尝试转换为Word文档。")
+        logging.error(e)
+        try:
+            output_file = f"{markdown_file.split('.')[0]}.docx"
+            pypandoc.convert_file(markdown_file, 'docx', outputfile=output_file, extra_args=[f'--resource-path={os.path.dirname(image_zip)}'])
+        except Exception as e:
+            logging.error(e)
+            output_file = None
+    # 删除解压出的图片文件夹,并且如编译成功则发送到下载区
+    if output_file:
+        promote_file_to_downloadzone(output_file, chatbot=chatbot)
+        generated_conclusion_files.append(output_file)
+    if os.path.exists(f"{os.path.dirname(image_zip)}/images/"):
+        shutil.rmtree(f"{os.path.dirname(image_zip)}/images/")
 
 def 解析PDF_基于NOUGAT(file_manifest, project_folder, llm_kwargs, plugin_kwargs, chatbot, history, system_prompt):
     import copy
@@ -256,15 +291,18 @@ def 解析PDF_基于NOUGAT(file_manifest, project_folder, llm_kwargs, plugin_kwa
                     else:
                         temp_onougat = yield from nougat_handle.NOUGAT_parse_pdf(file["path"], chatbot, history)
                         files_nougat.append(temp_onougat)
-                fpp = f"{fp.split('.')[0]}.mmd"
+                fpp = fp
                 with open(fpp, "w", encoding="utf-8") as result_file:
                     for file in files_nougat:
                         with open(file, "r", encoding="utf-8") as temp_file:
                             result_file.write(temp_file.read())
                             result_file.write("\n")
                         os.remove(file)
-                        # 顺便把拆分的pdf也删除了
-                        #os.remove(file.split('.')[0] + ".pdf")
+                for file in split_result:
+                    os.remove(file["path"])
+                image_folder = f"{fp.split('.')[0]}"
+                zip_images = zip_result(image_folder)
+                image_zip = promote_file_to_downloadzone(zip_images, rename_file=os.path.basename(zip_images)+'images.zip', chatbot=chatbot)
                 promote_file_to_downloadzone(fpp, rename_file=os.path.basename(fpp)+'.nougat.mmd', chatbot=chatbot)
             else:
                 chatbot.append(["当前进度：", f"正在解析论文，请稍候。（第一次运行时，需要花费较长时间下载NOUGAT参数）"]); yield from update_ui(chatbot=chatbot, history=history) # 刷新界面
@@ -278,8 +316,14 @@ def 解析PDF_基于NOUGAT(file_manifest, project_folder, llm_kwargs, plugin_kwa
         article_dict = markdown_to_dict(article_content)
         logging.info(article_dict)
         yield from translate_pdf(article_dict, llm_kwargs, chatbot, fp, generated_conclusion_files, TOKEN_LIMIT_PER_FRAGMENT, DST_LANG)
-
-    chatbot.append(("给出输出文件清单", str(generated_conclusion_files + generated_html_files)))
+        
+        if plugin_kwargs == "1":
+            markdown_file = generated_conclusion_files[1]
+            编译pdf(image_zip, markdown_file, chatbot, generated_conclusion_files)
+    if plugin_kwargs == "1":
+        chatbot.append(["如要在mmd文档中查看文件，请将压缩包中图片解压到同一目录下的‘images’文件夹中\n给出输出文件清单", str(generated_conclusion_files + generated_html_files)])
+    else:
+        chatbot.append(("给出输出文件清单", str(generated_conclusion_files + generated_html_files)))
     yield from update_ui(chatbot=chatbot, history=history) # 刷新界面
 
 
