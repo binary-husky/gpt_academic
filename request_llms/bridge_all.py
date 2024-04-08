0887c253cc8e40bfb82d2ba977348439cdaeb79b
binary-husky <qingxu.fu@outlook.com>
2024-04-08 11:39:56 +0800

    change cohere api endpoint

--- request_llms/bridge_all.py
+++ request_llms/bridge_all.py
@@ -67,7 +67,7 @@ newbing_endpoint = "wss://sydney.bing.com/sydney/ChatHub"
 gemini_endpoint = "https://generativelanguage.googleapis.com/v1beta/models"
 claude_endpoint = "https://api.anthropic.com/v1/messages"
 yimodel_endpoint = "https://api.lingyiwanwu.com/v1/chat/completions"
-cohere_endpoint = 'https://cohere.chatcoding.xyz/v1/chat'
+cohere_endpoint = 'https://api.cohere.ai/v1/chat'
 
 if not AZURE_ENDPOINT.endswith('/'): AZURE_ENDPOINT += '/'
 azure_endpoint = AZURE_ENDPOINT + f'openai/deployments/{AZURE_ENGINE}/chat/completions?api-version=2023-05-15'
